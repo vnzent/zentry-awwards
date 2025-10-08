f0b59e1fa88bc8555de2c143ef29a9e1a40a01fb
Vincent <vincent.ptk17@gmail.com>
2025-10-08 11:46:43 +0800

    fix: correct typo on w-screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from './components/Hero'
 
 const App = () => {
   return (
-    <main className="relative min-h-screen w-screnn overflow-x-hidden">
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
     </main>
   )
