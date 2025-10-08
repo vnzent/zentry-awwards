f1094087c44309e5d802539dd9f10fd355af4006
Vincent <vincent.ptk17@gmail.com>
2025-10-08 16:32:29 +0800

    add Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({ title, id, rightIcon, leftIcon, containerClass}) => {
+  return (
+    <button id={id} className="group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full">
+
+    </button>
+  )
+}
+
+export default Button
