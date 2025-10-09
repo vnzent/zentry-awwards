ed67d68b3568170395bb95d54b32357d531113a3
Vincent <vincent.ptk17@gmail.com>
2025-10-09 15:18:25 +0800

    create About content with Animated Title

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/all';
+import AnimatedTitle from './AnimatedTitle';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
+    useGSAP(() => {
+        const clipAnimation = gsap.timeline({
+             scrollTrigger: {
+                trigger: "#clip",
+                start: "center center",
+                end: "+=800 center",
+                scrub: 0.5,
+                pin: true,
+                pinSpacing: true
+             }
+        })
+
+        clipAnimation.to(".mask-clip-path", {
+            width: "100vw",
+            height: "100vh",
+            borderRadius: 0
+        })
+    })
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <h2 className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to Zentry
+        </h2>
+        <AnimatedTitle title="Discover the world's<br />largest shared adventure" containerClass="!text-black"/>
+        <div className="absolute bottom-[-80dvh] left-1/2 -translate-x-1/2 w-full max-w-96 text-center font-circular text-lg md:max-w-[34rem]">
+          <p>The Game of Games begins-your life, now an epic MMORPG</p>
+          <p>Zentry unites every player from countless games and platforms</p>
+        </div>
+      </div>
+      <div id="clip" className="h-dvh w-screen">
+        <div className="mask-clip-path absolute left-1/2 -translate-x-1/2 top-0 z-20 h-[60vh] w-96 md:w-[30vw] origin-center overflow-hidden rounded-3xl">
+          <img 
+            src="public/img/about.webp" 
+            alt="background" 
+            className="absolute left-0 top-0 size-full object-cover" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About
--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all';
 
-const AnimatedTitle = () => {
+gsap.registerPlugin(ScrollTrigger)
+
+const AnimatedTitle = ({title, containerClass}) => {
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        const ctx = gsap.context(() => {
+            const titleAnimation = gsap.timeline({
+                scrollTrigger: {
+                    trigger: containerRef.current,
+                    start: "100 bottom",
+                    end: "center bottom",
+                    toggleActions: "play none none reverse"
+                }
+            })
+
+            titleAnimation.to(".animated-word", {
+                opacity: 1,
+                transform: "translate3d(0,0,0) rotateY(0deg) rotateX(0deg)",
+                ease: "power2.inOut",
+                stagger: 0.05,
+            })
+        }, containerRef)
+
+        return () =>  ctx.revert();
+    }, [])
   return (
-    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-8xl ">
-      Discover the world's largest shared adventure
+    <div 
+        ref={containerRef}
+        className={`flex flex-col gap-1 text-7xl uppercase leading-[.8] mt-5 text-center text-white sm:px-32 md:text-[6rem] ${containerClass}`}>
+        {title.split('<br />').map((line, index) => (
+            <div key={index} className="flex justify-center items-center max-w-full flex-wrap gap-2 px-10 md:gap-3">
+                {line.split(" ").map((word, i) => (
+                    <span key={i} className="animated-word" dangerouslySetInnerHTML={{ __html: word }}/>
+                ))}
+            </div>
+        ))}
     </div>
   );
 }
