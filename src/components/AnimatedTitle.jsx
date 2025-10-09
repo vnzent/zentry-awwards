a91957ddb64c0dcfd49e6d909982fe1bf23cc220
Vincent <vincent.ptk17@gmail.com>
2025-10-09 10:55:03 +0800

    create new component for title animation

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AnimatedTitle = () => {
+  return (
+    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-8xl ">
+      Discover the world's largest shared adventure
+    </div>
+  );
+}
+
+export default AnimatedTitle
