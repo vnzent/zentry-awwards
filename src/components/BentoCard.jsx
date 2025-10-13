12f5ae6125eb52028c3eb0426ed9a299818fcbf2
Vincent <vincent.ptk17@gmail.com>
2025-10-13 11:02:45 +0800

    create layout for BentoCard component

--- src/components/BentoCard.jsx
+++ src/components/BentoCard.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
-const BentoCard = () => {
+const BentoCard = ({ src, title, description, isComingSoon}) => {
   return (
-    <div>BentoCard</div>
+    <div className="relative size-full">
+        <video 
+            src={src}
+            loop
+            muted
+            autoPlay
+            className="absolute left-0 top-0 size-full object-cover object-center"
+        />
+        <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+            <div>
+                <h1 className="uppercase font-zentry font-black text-4xl md:text-6xl">{title}</h1>
+                {description && (
+                    <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
+                )}
+            </div>
+        </div>
+    </div>
   )
 }
 
