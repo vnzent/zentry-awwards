9ba878ac9e78a17b09c6a9378353c8e8640dda38
Vincent <vincent.ptk17@gmail.com>
2025-10-13 12:29:31 +0800

    add new component Contact.jsx and import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from './components/About'
 import Navbar from './components/Navbar'
 import Features from './components/Features'
 import Story from './components/Story'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <About />
       <Features />
       <Story />
+      <Contact />
     </main>
   )
 }
